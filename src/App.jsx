c2695b7ef057b42cd4779f3a302da71b06d7e8dd
hossein rahmati <rmthossein@gmail.com>
2024-01-31 17:51:30 +0330

    add features-base driven folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/auth/auth";
 
 function App() {
-  return <div>برنامه</div>;
+  return (
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
 }
 
 export default App;
